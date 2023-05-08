b41df5139ffd27ca86192073e42989afb0568549
Tuong <aumanhtuong09122001@gmail.com>
2023-05-08 09:40:04 +0700

    xoa demo cua Huy

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @RestController
 @CrossOrigin
-@RequestMapping("users")
+@RequestMapping("user")
 public class UserController {
     @Autowired
     private IUserService userService;
